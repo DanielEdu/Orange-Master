d7ac059f2d6db17fc4b9c5e0c6a1d42dbd33dbb5
Daniel Portugal <gx.pinwii@gmail.com>
2014-12-16 15:12:07 -0500

    mm

--- api/controllers/FileController.js
+++ api/controllers/FileController.js
@@ -11,7 +11,7 @@ module.exports = {
 
     res.writeHead(200, {'content-type': 'text/html'});
     res.end(
-    '<form action="http://localhost:1337/file/upload" enctype="multipart/form-data" method="post">'+
+    '<form action="http://localhost:1337/file/upload" enctype="multipart/form-data" method="POST">'+
     '<input type="text" name="title"><br>'+
     '<input type="file" name="avatar" multiple="multiple"><br>'+
     '<input type="submit" value="Upload">'+
@@ -20,18 +20,27 @@ module.exports = {
   },
 
   upload: function  (req, res) {
-    var uploadPath = 'assets/file/';
-  	req.file('avatar').upload(function onUploadComplete (err, files) {             
-		if (err)
-			return res.serverError(err);
-    console.log(files);
-
-	    
-	 });
+      req.file('avatar').upload(function (err, uploadedFiles) {
+        if (err) return res.send(500, err);
+        return res.json({
+          message: uploadedFiles.length + ' file(s) uploaded successfully!',
+          files: uploadedFiles
+        });
+      });
   },
 
   show: function(req, res) {
-  	res.attachment('complementario');
+  	var fs = require('fs');
+    var filePath = 'tmp/uploads/';
+    var stat = fs.statSync(filePath);
+
+    response.writeHead(200, {
+        
+        'Content-Length': stat.size
+    });
+
+    var readStream = fs.createReadStream(filePath);
+    readStream.pipe(response);
   }
 
 };
